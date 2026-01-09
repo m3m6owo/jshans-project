383c1c397b9365e9c531d859f11785e53bbbaa7f
jshan <jshan0309@hotmail.com>
2026-01-09 11:09:06 +0800

    fix. vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jshans-project/',
+  base: '/jshans-project',
   build: {
     outDir: 'dist',
   },
